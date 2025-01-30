22cd47366a8422656be0bcba4982b5dd1c875ae2
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-01-30 14:19:13 -0300

    Update ContaBancaria.java

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+    public String titular;
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 }
